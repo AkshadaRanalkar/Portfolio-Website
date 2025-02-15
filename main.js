9fe73f96aa8ad876503531ad95fe25ed1de21d74
Akshada Ranalkar <akshadaranalkar27@gmail.com>
2025-02-15 12:09:39 -0600

    Removed old website files

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed= new Typed(".text", {
-    strings:["Computer Science Master's Student" , "Creative Thinker" , "Coder"],
-    typeSpeed:50,
-    backSpeed:50,
-    backDelay:1000,
-    loop:true
-})
